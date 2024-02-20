a19044d98502f7e5cc6cbe6f96bd4590a129dc0f
Shakil4432 <shakil4432@gmail.com>
2024-02-21 00:10:54 +0600

    full project

--- app.js
+++ app.js
@@ -18,28 +18,30 @@ const setupButtonClick = (buttonId, categoryId) => {
 const displayData = (data) => {
   const videoContainer = document.getElementById("video_container");
   videoContainer.innerHTML = "";
+
   if (data.length === 0) {
     const ins = document.createElement("div");
     ins.classList.add("No_data");
     ins.innerHTML = `
-    <img class="icon_img" src="icon.png" alt="">
-    <h1>OOPS!! Sorry, There is No Content Here.</h1>
+      <img class="icon_img" src="icon.png" alt="">
+      <h1>OOPS!! Sorry, There is No Content Here.</h1>
     `;
     videoContainer.appendChild(ins);
   } else {
+    data.sort((a, b) => b.others.views - a.others.views);
+
     data.forEach((video) => {
       console.log(video);
       const videoCard = document.createElement("div");
       videoCard.classList.add("item_card");
-      
-      // Check if the video is verified
+
+      let verifiedIcon = "";
       if (video.authors[0].verified === true) {
-        videoCard.innerHTML = `
-          <span><i class="fa-solid fa-check"></i></span>
-        `;
+        verifiedIcon = `<span><i class="fa-solid fa-check"></i></span>`;
       }
-      
-      videoCard.innerHTML = videoCard.innerHTML + `
+
+      videoCard.innerHTML = `
+        ${verifiedIcon}
         <div class="Card_details">
           <img class="img_div" src="${video.thumbnail}" alt="">
           <img class="img_profile" src="${video.authors[0].profile_picture}" alt="">
@@ -48,11 +50,16 @@ const displayData = (data) => {
           <h5 class="view">${video.others.views}</h5>
         </div>
       `;
-      
+
       videoContainer.appendChild(videoCard);
     });
   }
 };
+const addBlog = document
+  .getElementById("blog")
+  .addEventListener("click", () => {
+    window.location.href = "index2.html";
+  });
 
 setupButtonClick("allButton", 1000);
 setupButtonClick("musicButton", 1001);
